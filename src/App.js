b80a011d83ce71d27cccdec52d16f95ad896c808
Andre Stackhouse <CaptainStack@outlook.com>
2017-04-10 01:03:33 -0700

    Homepage restructure.

--- src/App.js
+++ src/App.js
@@ -8,22 +8,32 @@ class App extends Component {
         <header>
           <h2><a href='.'>TRUCK STOP</a></h2>
         </header>
-        <div id='main-content'>
-          <div id='stops-box'>
-            <h1>Stops</h1>
-            <ul>
-              <li>Seattle</li>
-              <li>Portland</li>
-              <li>San Francisco</li>
-            </ul>
-          </div>
-          <div id='tasks-box'>
-            <h1>Tasks</h1>
-            <ul>
-              <li>Drop off deliveries</li>
-              <li>Refill gas tank</li>
-              <li>Call home</li>
-            </ul>
+        <div id='main-container'>
+          <div id='app-container'>
+            <div id='jobs-box'>
+              <h1>Jobs</h1>
+              <ul>
+                <li>Seattle</li>
+                <li>Portland</li>
+                <li>San Francisco</li>
+              </ul>
+            </div>
+            <div id='stops-box'>
+              <h1>Stops</h1>
+              <ul>
+                <li>Drop off deliveries</li>
+                <li>Refill gas tank</li>
+                <li>Call home</li>
+              </ul>
+            </div>
+            <div id='tasks-box'>
+              <h1>Tasks</h1>
+              <ul>
+                <li>Drop off deliveries</li>
+                <li>Refill gas tank</li>
+                <li>Call home</li>
+              </ul>
+            </div>
           </div>
           <footer>
             <div><a href='https://captainstack.github.io/public-stackhouse' target='_blank'><p>© 2017 by Andre Stackhouse</p></a></div>
