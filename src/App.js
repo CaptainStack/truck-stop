f25c86384173f23040dca7370d7855fc8a6e0c6e
Andre Stackhouse <CaptainStack@outlook.com>
2017-04-13 08:53:40 -0700

    Refactor styles and fonts into directories.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ChecklistManager from './components/ChecklistManager';
-import './App.css';
+import './styles/App.css';
 
 const App = ({state}) => {
   return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './styles/index.css';
 import {createStore} from 'redux';
 import reducer from './reducer';
 
