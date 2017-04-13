3ebc327c2619e2195434bc53e9db56f531ffa584
Andre Stackhouse <CaptainStack@outlook.com>
2017-04-13 08:26:49 -0700

    Remove deprecated events/reducers.

--- src/events.js
+++ src/events.js
@@ -1,17 +1,5 @@
 import {store} from './index'
 
-export const jobClick = clicked_job => e => {
-  if (clicked_job !== store.getState().selected_job) {
-    store.dispatch({ type: 'UPDATE_SELECTED_JOB', job: clicked_job });
-  }
-}
-
-export const stopClick = clicked_stop => e => {
-  if (clicked_stop !== store.getState().selected_stop) {
-    store.dispatch({ type: 'UPDATE_SELECTED_STOP', stop: clicked_stop });
-  }
-}
-
 export const taskClick = clicked_task => e => {
   navigator.geolocation.getCurrentPosition(position => {
     store.dispatch({ type: 'UPDATE_SELECTED_TASK', task: clicked_task, GPS: { latitude: position.coords.latitude, longitude: position.coords.longitude} });
--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,7 @@
-import {update_active_job, update_active_stop, update_clicked_task, update_task_filter, add_image_to_task, INITIAL_STATE} from './core';
+import {update_clicked_task, update_task_filter, add_image_to_task, INITIAL_STATE} from './core';
 
 export default function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case 'UPDATE_SELECTED_JOB':
-      return update_active_job(state, action.job);
-    case 'UPDATE_SELECTED_STOP':
-      return update_active_stop(state, action.stop);
     case 'UPDATE_SELECTED_TASK':
       return update_clicked_task(state, action.task, action.GPS);
     case 'UPDATE_FILTER':
